8308e32a81538bb21b8b80feb2fd5497ce2381fe
DavidVoe125 <david.voellmar@gmail.com>
2021-09-17 23:01:03 +0200

    Scene load system

--- Assets/Scripts/Scripts/PersistanceScripts/Spawnscript.cs
+++ Assets/Scripts/Scripts/PersistanceScripts/Spawnscript.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_Scenes : MonoBehaviour
+public class Spawnscript : MonoBehaviour
 {
+    public bool isSpawnable = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!isSpawnable)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scripts/Safetestschreiben.cs
+++ Assets/Scripts/Scripts/Safetestschreiben.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_Textures : MonoBehaviour
+public class Safetestschreiben : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,9 @@ public class Test_Textures : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.F))
+        {
+           // PlayerPrefs.SetFloat("Testfloat", 2.5f);
+        }
     }
 }
